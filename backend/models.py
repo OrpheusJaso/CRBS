82791e9ef26f644299f69eea03c74b2c19722ac6
Wan Wei Siang <jasonwan9937@gmail.com>
2026-06-09 16:18:15 +0800

    Setup

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id         = db.Column(db.Integer, primary_key=True)
+    title      = db.Column(db.String(200), nullable=False)
+    content    = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
